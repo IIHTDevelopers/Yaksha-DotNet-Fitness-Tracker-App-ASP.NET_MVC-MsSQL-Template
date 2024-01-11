597bb5d30719c9e42cda700b552f3c2f8921fe9d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 10:39:43 +0530

    Add files via upload

---
+++ FitnessTrackerApp/Controllers/FitnessTrackerController.cs
@@ -0,0 +1,77 @@
+﻿using FitnessTrackerApp.DAL.Interface;
+using FitnessTrackerApp.DAL.Repository;
+using FitnessTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessTrackerApp.Controllers
+{
+    public class FitnessTrackerController : Controller
+    {
+        private readonly IFitnessTrackerInterface _Repository;
+        public FitnessTrackerController(IFitnessTrackerInterface service)
+        {
+            _Repository = service;
+        }
+        public FitnessTrackerController()
+        {
+            // Constructor logic, if needed
+        }
+        // GET: FitnessTracker
+        public ActionResult Index()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ViewResult Details(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Create()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Workout workout)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult EditAsync(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Workout workout)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FitnessTrackerApp/DAL/Repository/FitnessTrackerRepository.cs
@@ -0,0 +1,61 @@
+﻿using FitnessTrackerApp.DAL.Interface;
+using FitnessTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FitnessTrackerApp.DAL.Repository
+{
+    public class FitnessTrackerRepository : IFitnessTrackerRepository
+    {
+        private FitnessTrackerDbContext _context;
+        public FitnessTrackerRepository(FitnessTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Workout> GetWorkouts()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Workout GetWorkoutByID(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Workout InsertWorkout(Workout workout)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public int DeleteWorkout(int workoutID)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public bool UpdateWorkout(Workout workout)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Save()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
